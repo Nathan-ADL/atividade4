62c28662a03521f5fbee4e6a2df8d2b70669063f
Nathan Ariel <nathanarieldamasioleao@gmail.com>
2026-06-16 19:12:22 -0300

    add: mudanças para integração com docker

--- app.py
+++ app.py
@@ -644,4 +644,4 @@ def api_agenda_get(barbeiro_id):
         return jsonify({"ok": False, "erro": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run( host="0.0.0.0",port=5000, debug=True)
--- database.sql
+++ database.sql
@@ -68,34 +68,3 @@ CREATE TABLE IF NOT EXISTS disponibilidade_excecao (
 
 ALTER TABLE agendamento DROP FOREIGN KEY agendamento_ibfk_3;
 ALTER TABLE agendamento DROP COLUMN servico_id;
-
-INSERT INTO servicos (nome, preco, descricao, duracao_min) VALUES
-('Sobrancelha', 20.00, 'Design com fio · 15 min', 15),
-('Corte', 45.00, 'Corte tradicional · 30 min', 30),
-('Barba', 35.00, 'Navalha e toalha quente · 25 min', 25),
-('Corte + Barba', 70.00, 'Combo completo · 55 min', 55),
-('Hidratação', 60.00, 'Tratamento capilar · 40 min', 40);
-
-INSERT INTO barbeiro (nome, especialidade) VALUES
-('Ricardo C.', 'Clássico & moderno'),
-('Felipe M.', 'Especialista em barba'),
-('André G.', 'Coloração & estilo'),
-('João P.', 'Cortes modernos');
-
-INSERT INTO disponibilidade (barbeiro_id, dia_semana, hora_inicio, hora_fim) VALUES
-
--- Ricardo C. (id = 1)
-(1, 1, '08:00:00', '18:00:00'), -- Segunda
-(1, 2, '08:00:00', '18:00:00'), -- Terça
-(1, 3, '08:00:00', '18:00:00'), -- Quarta
-(1, 4, '08:00:00', '18:00:00'), -- Quinta
-(1, 5, '08:00:00', '18:00:00'), -- Sexta
-(1, 6, '08:00:00', '14:00:00'), -- Sábado
-
--- Felipe M. (id = 2)
-(2, 1, '09:00:00', '19:00:00'), -- Segunda
-(2, 2, '09:00:00', '19:00:00'), -- Terça
-(2, 3, '09:00:00', '19:00:00'), -- Quarta
-(2, 4, '09:00:00', '19:00:00'), -- Quinta
-(2, 5, '09:00:00', '19:00:00'), -- Sexta
-(2, 6, '09:00:00', '15:00:00'); -- Sábado
